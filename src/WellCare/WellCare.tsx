1020c56e18b67d0141047bb286074e6d679cad67
Tapan Upadhyay <tapanupa4321@gmail.com>
2025-09-12 17:08:30 +0530

    agent-wellcares

--- src/WellCare/WellCare.tsx
+++ src/WellCare/WellCare.tsx
@@ -18,7 +18,7 @@ export default function WellCareBasic() {
   });
 
   // --- Your keys ---
-  const RETELL_API_KEY = "53b76c26-bd21-4509-98d7-c5cc62f93b59";
+  const RETELL_API_KEY = "02e501b4-1b05-40f4-af3e-351f0819e13f";
   const RETELL_AGENT_ID = "agent_6314194ec8912f8a60f36f4538";
   // ------------------
 
